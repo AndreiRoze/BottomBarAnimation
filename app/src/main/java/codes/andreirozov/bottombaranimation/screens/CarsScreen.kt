ef006c617263f54e16441cd12eb81ad7aeed995f
Andrei <andrei.rozov98@gmail.com>
2022-02-10 15:28:46 +0100

    Improve glow effect for lazyColumn, delete unused things, update dependencies

--- app/src/main/java/codes/andreirozov/bottombaranimation/screens/CarsScreen.kt
+++ app/src/main/java/codes/andreirozov/bottombaranimation/screens/CarsScreen.kt
@@ -1,47 +1,51 @@
 package codes.andreirozov.bottombaranimation.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.LocalOverScrollConfiguration
+import androidx.compose.foundation.gestures.OverScrollConfiguration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CarsScreen(navController: NavController) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(horizontal = 8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize(),
+    // Use CompositionLocalProvider to create custom OverScrollConfiguration with vertical padding,
+    // otherwise glow effect fill be under TopBar and BottomBar
+    CompositionLocalProvider(
+        LocalOverScrollConfiguration provides OverScrollConfiguration(
+            drawPadding = PaddingValues(vertical = 56.dp)
+        ),
         content = {
-            // Spacer for topBar, otherwise content will be covered by bottomBar
-            item {
-                Spacer(modifier = Modifier.height(56.dp))
-            }
+            LazyColumn(
+                // Vertical content padding is 64dp, because 56dp is height of TopBar and BottomBar + 8dp visual padding
+                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 64.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                content = {
+                    //Title text
+                    item {
+                        Text(text = "CARS SCREEN")
+                    }
 
-            //Title text
-            item {
-                Text(text = "CARS SCREEN")
-            }
-
-            // 20 cards with content
-            items(20) {
-                CarCard(
-                    navController = navController,
-                    name = "Car card number is $it"
-                )
-            }
-
-            // Spacer for bottomBar, otherwise content will be covered by bottomBar
-            item {
-                Spacer(modifier = Modifier.height(56.dp))
-            }
+                    // 20 cards with content
+                    items(20) {
+                        CarCard(
+                            navController = navController,
+                            name = "Car card number is $it"
+                        )
+                    }
+                }
+            )
         }
     )
 }
